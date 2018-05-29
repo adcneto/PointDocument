7024c1f0f6f5937193c13391505199e88fce0575
adcneto <39680902+adcneto@users.noreply.github.com>
2018-05-29 13:08:25 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,17 +3,17 @@ import random
 
 @match_regex(r'registry novo|novo registry|registry|new registry|faq do registry|docs do registry', case_sensitive=False)
 async def newregistry(opsdroid, config, message):
-    await message.respond('https://confluence.intranet.uol.com.br/confluence/display/PagSeguro/FAQ+-+Docker+Registry')
+    await message.respond('y')
 
 @match_regex(r'exit 34|exit status 34|erro 34')
 async def erro34 (opsdroid, config, message):
     if match_regex(r'exit 34|exit status 34|erro 34') == "exit 34":
         message.respond("Tudo bem, meu bacano? \n")
-    await message.respond('https://confluence.intranet.uol.com.br/confluence/pages/viewpage.action?pageId=176849282')
+    await message.respond('')
 
 @match_regex(r'amazon web services|aws', case_sensitive=False)
 async def faq_aws (opsdroid, config, message):
-    await message.respond('https://confluence.intranet.uol.com.br/confluence/display/PagSeguro/FAQ')
+    await message.respond('h')
 
 
 @match_regex(r'(?:aws|amazon web services) (?:erro 34|exit status 34)|(?:erro 34|exit status 34) (?:aws|amazon web services)', case_sensitive=False)
